904ae8b09f6d909bf30d0d16a17317fc7fae9f1f
Ghost1386 <71891421+Ghost1386@users.noreply.github.com>
2022-02-21 21:26:31 +0300

    Add One Controller

--- HospitalManagmentSystem/Controllers/UserController.cs
+++ HospitalManagmentSystem/Controllers/UserController.cs
@@ -1,10 +1,14 @@
-﻿using HospitalManagmentSystem.BusinessLogic.Interfaces;
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using HospitalManagmentSystem.BusinessLogic.Interfaces;
 using HospitalManagmentSystem.Common.Enums;
 using HospitalManagmentSystem.Common.Models;
 using HospitalManagmentSystem.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Serilog;
 
 namespace HospitalManagmentSystem.Controllers
 {
